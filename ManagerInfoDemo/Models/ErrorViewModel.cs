426ea551dd8bb521af420d72d59c820d7b19749a
LamNV290304 <nguyenvietlam290304@gmail.com>
2025-10-30 06:16:51 +0700

    First commit

---
+++ ManagerInfoDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ManagerInfoDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
